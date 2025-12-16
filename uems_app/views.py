6959ff88c4aed62950105854ead9eeef76669922
rainiervan <rainiervan.panganiban@gmail.com>
2025-12-16 19:06:28 +0800

    Changed authentication method to email on login

--- uems_app/views.py
+++ uems_app/views.py
@@ -29,15 +29,16 @@ def is_attendee(user):
 @never_cache
 def login_view(request):
     if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        user = authenticate(username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect("dashboard")
-        else:
-            messages.error(request, "Invalid username or password.")
+        try:
+            password = request.POST.get("password")
+            user = authenticate(username=User.objects.get(email=request.POST.get("email")).username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("dashboard")
+            else:
+                messages.error(request, "Invalid username or password.")
+        except ObjectDoesNotExist:
+            messages.error(request, "User does not exist.")
     elif request.user.is_authenticated:
         return redirect("dashboard")
 
